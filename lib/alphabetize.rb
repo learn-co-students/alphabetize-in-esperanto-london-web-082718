4c3456ee810989594d01056780351f3f11ac5dd6
thexyman <makdaa1@gmail.com>
2018-08-14 12:32:13 +0100

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
 def alphabetize(arr)
   # code here
+
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  sorted = arr.sort_by do |word|
+  word.split('').map do |letter|
+    alphabet.index(letter)
+  	end
+	end
+
+	sorted
+
 end
